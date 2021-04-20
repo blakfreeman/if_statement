96acc6b89f4422d7c5cea5435c1de4e4bdacf3fe
blakfreeman <61966557+blakfreeman@users.noreply.github.com>
2021-04-20 09:52:17 +0200

    if you know you know

---
+++ main.js
@@ -0,0 +1,124 @@
+// let y = 1
+
+
+
+
+// console.log(1==1);
+// console.log(1===1);
+
+//  let correct = 'correct'
+//  let not  = 'pas correct'
+
+//  let nom = prompt('what is your name');
+
+// if (nom.length < 5) {
+//     alert('your name is to short')
+// }
+
+
+// let addition = prompt('combien fait 3+2');
+
+// if (addition == 5) {
+//     alert('great job you made it')
+// }
+// else {
+//     alert(' think better next time')
+// }
+
+// let multi = prompt('3X5')
+// let bad = multi - 15;
+
+// if ( multi == 15) {
+//     prompt('you really goood')
+// }
+
+
+// else{
+//     prompt(bad +' ' + ' almost')
+// }
+
+//6
+
+// let tab = [];
+
+// prompt(tab);
+// prompt(tab);
+// prompt(tab);
+
+// if (tab.length = 3) {
+//     alert('this table contain  3 val ' + tab);
+// }
+
+
+
+// var grandNombres = [];
+// var petitNombres = [];
+
+// var nomb = prompt('type a number'); 
+
+// if ( nomb > 12) {
+//     grandNombres.push(nomb);
+// }
+// else if ( nomb < 12 ) {
+//     petitNombres.push(nomb);
+// }
+
+// else if ( nomb == 12) {
+//     grandNombres.push(nomb);
+
+// }
+
+// var nomb = prompt('type a new number'); 
+
+// if ( nomb > 12) {
+//     grandNombres.push(nomb);
+// }
+// else if ( nomb < 12 ) {
+//     petitNombres.push(nomb);
+// }
+
+// else if ( nomb == 12) {
+//     grandNombres.push(nomb);
+
+// }
+
+// alert( 'le petit contient ' + petitNombres + 'le grand contient ' + grandNombres);
+
+
+// var lastName;
+// var famillyName;
+// var age; 
+
+// lastName = prompt('what is your lastname');
+
+// famillyName = prompt('what is your familly name');
+
+// age = prompt('how old are you ?');
+
+// if ( lastName == 'anthony' && famillyName == 'pemba' && age > 18) {
+//     alert('you are the one true king ' + lastName +" "+ famillyName )
+
+// }
+
+// else 
+// {
+//     alert('welcome ' + lastName+ ' ' + famillyName )
+// }
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
